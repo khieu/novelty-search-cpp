b7766b08e16790647d7bd028894c5919fa710f01
khieu <hieule246@gmail.com>
2017-02-08 23:06:35 -0800

    Add stats functions, add ignore files to .gitnignore

---
+++ stats.cpp
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+double average(vector<double> data) {
+  double avg = 0;
+  for (int i = 0; i < data.size(); i++) {
+    avg += data[i];
+  }
+
+  avg = avg/data.size();
+  return avg;
+}
+
+
+double range(vector<double> data) {
+  // find max in array
+  double max = data[0];
+  for (int i = 0; i < data.size(); i++) {
+    if (data[i] > max) {
+      max = data[i];
+    } 
+  }
+  // find min in array
+  double min = data[0];
+  for (int i = 0; i < data.size(); i++) {
+    if (data[i] < min) {
+      min = data[i];
+    } 
+  }
+  // return max - min
+  return (double) (max - min);
+}
+
+double innerQuartileRange(vector<double> data) {
+  double q1,q3;
+  //sort data
+  std::sort(data.begin(), data.end());
+  // find median
+  int halfSize = (int) data.size()/2;
+  // find q1, q3
+  if (halfSize % 2 == 1) {
+    q1 = data[(int) halfSize/2];
+    q3 = data[data.size() - ((int) halfSize/2)-1];
+  } else {
+    q1 = (data[(int) halfSize/2] + data[(int) halfSize/2 - 1] )/ 2 ;
+    q3 = (data[data.size() - (int) halfSize/2] + data[data.size() - 1 - (int) halfSize/2])/2;
+  }
+  // return
+  return q3 - q1;
+}
+
+double meanAbsoluteDeviation(vector<double> data) {
+  double avg = average(data), mad = 0;
+
+  for (int i = 0; i < data.size(); i++) {
+    mad += abs(data[i] - avg);
+  }
+  mad = mad/data.size();
+
+  return mad;
+}
+
+double variance(vector<double> data) {
+  double avg = average(data), variance = 0;
+
+  for (int i = 0; i < data.size(); i++) {
+    variance += pow((data[i] - avg), 2);
+  }
+  variance = (double) variance / data.size();
+  return variance;
+}
+
+double standardDeviation(vector<double> data) {
+  double vrnc = variance(data);
+  double stdDeviation = sqrt(vrnc);
+  return stdDeviation;
+}
+
+double entropy(vector<double> data) {
+  // calculate # of appearance of each point in the data vector
+  map<double, double> count;
+  for (int i = 0; i < data.size(); i++) {
+    if (count.find(data[i]) == count.end()) {
+      count.insert(pair<double, double>(data[i],1));
+    } else {
+      map<double,double>::iterator it = count.find(data[i]);
+      it->second = it->second + 1;
+    }
+  }
+  int size = data.size();
+  // calculate probability
+  for (auto& it : count) {
+    it.second = it.second/size;
+  }
+  // calculate info entropy
+  double sum = 0;
+  for (auto& it : count) {
+    double pi = it.second;
+    sum += pi*log2(pi);
+  }
+  sum = -sum;
+
+  return sum;
+}
+
+int main( int argc, const char* argv[] )
+{
+  printf( "\nHello World\n\n" );
+  vector<double> data = {1,2,25,3,5,6,3,4, 12, 18};
+  double x = innerQuartileRange(data);
+  cout << x << endl;
+  vector<double> x1 = {2,2,4,4};
+  vector<double> x2 = {1,1,6,4};
+  cout << meanAbsoluteDeviation(x1) << endl;
+  cout << meanAbsoluteDeviation(x2) << endl;
+  vector<double> x3 = {2,4,4,4,5,5,7,9};
+  double vrnc = variance(x3);
+  double stdDeviation = standardDeviation(x3);
+  cout << vrnc << endl;
+  cout << stdDeviation << endl;
+  vector<double> x4 = {1,1,2,3};
+  cout << entropy(x4) << endl;
+}
